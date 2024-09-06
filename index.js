621e0a7ceae5fff9223fddb83919df8c6542369f
aditya_baniya <itsadityabaniya@gmail.com>
2024-09-06 11:33:52 -0500

    all the files are uploaded and updated

---
+++ index.js
@@ -0,0 +1,69 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+var i=0;
+
+//button press
+while (i<numberOfDrums){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        // this.style = "color:white";
+
+        var buttonHtml = this.innerHTML;
+        makeSound(buttonHtml);
+    });
+
+    i++;
+}
+
+//key board press
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+});
+
+// function to make sound
+
+function makeSound(buttonHtml){
+    switch (buttonHtml) {
+        case "w":
+            var audio = new Audio('./sounds/tom-1.mp3')
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio('./sounds/tom-2.mp3')
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio('./sounds/tom-3.mp3')
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio('./sounds/tom-4.mp3')
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio('./sounds/crash.mp3')
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio('./sounds/kick-bass.mp3')
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio('./sounds/snare.mp3')
+            audio.play();
+            break;
+
+        case "m":
+            var audio = new Audio('./sounds/snare.mp3')
+            audio.play();
+            break;
+    
+        default:
+            console.log(buttonHtml)
+            break;
+    }
+}
